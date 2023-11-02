e668e91f0914eeec030539e22848a3043fef81ee
Dominik Adis <dominik@adis-online.de>
2023-11-02 15:18:10 +0100

    fixed urls

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include  # add this
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
+
+    path("", include("apps.data_endpoint.urls")),
+
     # Auth routes - login / register
     path("", include("apps.authentication.urls")),
 
