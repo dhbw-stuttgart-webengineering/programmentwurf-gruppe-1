7349c733b481cc1d062d08ec2f71ae8067f30dde
Jeremias Hiller <138399312+jereinsky@users.noreply.github.com>
2023-11-10 09:22:33 +0100

    ?

--- core/urls.py
+++ core/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path("", include("apps.authentication.urls")),
 
     # Leave `Home.Urls` as last the last line
-    path("", include("apps.home.urls"))]
+    path("", include("apps.home.urls")),
+    
+    
+    ]
