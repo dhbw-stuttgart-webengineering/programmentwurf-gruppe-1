0d48798b77bf6014c05bebba408cb3d4d72b8644
Vanilla2207 <v.wettori@yahoo.com>
2023-11-10 12:46:56 +0100

    Merge remote-tracking branch 'origin/main' into feature/vanessa
    # Conflicts:
    #	.gitignore

--- apps/data_endpoint/save_to_bank.py
+++ apps/data_endpoint/save_to_bank.py
@@ -15,11 +15,6 @@ def save_dates(email_id,
                semester,
                unit):
     """save the data from dualis in the database"""
-    unit_id = unit["id"]
-    unit_name = unit["name"]
-    unit_credits = unit["credits"]
-    unit_grade_first_attempt = unit["grade_first_attempt"]
-    unit_grade_second_attempt = unit["grade_second_attempt"]
 
     module_to_save, created = Module.objects.get_or_create(module_id=id_module)
 
@@ -31,18 +26,18 @@ def save_dates(email_id,
         module_to_save.credits = credits_
         module_to_save.save()
 
-    unit_to_save, created = Unit.objects.get_or_create(unit_id=unit_id)
+    unit_to_save, created = Unit.objects.get_or_create(unit_id=unit["id"])
 
     if created:
-        unit_to_save.unit_id = unit_id
-        unit_to_save.unit_name = unit_name
-        unit_to_save.credits = unit_credits
+        unit_to_save.unit_id = unit["id"]
+        unit_to_save.unit_name = unit["name"]
+        unit_to_save.credits = unit["credits"]
         unit_to_save.id_of_module = module_to_save
         unit_to_save.save()
 
     student_grades, _ = Grade.objects.get_or_create(email_id=email_id, id_of_unit=unit_to_save)
     student_grades.name = email_id
-    student_grades.grade_first_attempt = unit_grade_first_attempt
-    student_grades.grade_second_attempt = unit_grade_second_attempt
+    student_grades.grade_first_attempt = unit["grade_first_attempt"]
+    student_grades.grade_second_attempt = unit["grade_second_attempt"]
     student_grades.id_of_unit = unit_to_save
     student_grades.save()
