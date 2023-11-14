073d1dcb5728c16d5f7801c180e9dd399e4fd3ef
Dominik Adis <110486189+domiderpanda@users.noreply.github.com>
2023-11-14 08:21:46 +0100

    Update data analysis and visualization features (#85)
    * ?
    
    * Durchfallquote
    
    * andere werte
    
    * mit zwei Dahsboards
    
    * kursvergleich dashboard
    
    * besser
    
    * verbesserung und fixes für Kursvergleich
    
    * Farbe der Balken random
    
    * ?
    
    * delete
    
    * eigene Noten ansehen und urls und views updated
    
    * neu
    
    * ?
    
    * weiße ränder
    
    * index aktuallisiert und view und urls gefixed
    
    * index uptdate diagramme immer neu bei klick
    
    * Updated
    
    * update
    
    * line too long fix
    
    * gelöscht und index uptdatr
    
    * sollte jetzt passen
    
    * eigenen noten ansehen funktioniert
    
    * funktioniert mit eigenen Noten
    
    * add calculate failure rate
    
    * Fix Grade Distribution
    
    * übergabe werte und failure rate wird angezeigt
    
    * index neues diagramm notenverteilung
    
    * Doppelte einträge gelöscht
    
    * funktioniert
    
    * jetzt
    
    * dropdown menü funktioniert
    
    * Sort Grade Distribution
    
    * neu
    
    * shes a beauty now
    
    ---------
    
    Co-authored-by: Jeremias Hiller <138399312+jereinsky@users.noreply.github.com>
    Co-authored-by: Jeremias Hiller <inf22153@lehre.dhbw-stuttgart.de>
    Co-authored-by: Johannes Peters <peters.j.jun@gmail.com>

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,12 +1,17 @@
 """Views for the home app"""
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest, JsonResponse
 from django.template import loader
 from django.urls import reverse
 from django.views.decorators.cache import cache_control
 from django.conf import settings
+from apps.data_endpoint.calculate_average import calculate_total_average_weighted
+from apps.data_endpoint.utils.grade_distribution import get_grade_distribution_as_dict
+from apps.data_endpoint.utils.failure_rate import get_failure_rate_first_attempt, get_passing_rate_first_attempt
+from apps.data_endpoint.read_data import get_grades
 from ..utils.decorators import refresh_dualis
+import json
 
 
 @login_required(login_url="/login/")
@@ -21,14 +26,41 @@ def index(request: HttpRequest) -> HttpResponse:
     Returns:
         HttpResponse: HttpResponde Object
     """
-    # Beispiel für die Verwendung von eigenen Daten
-    own_grades = [
-        {'course': 'Mathematik', 'grade': 2.7},
-        {'course': 'Englisch', 'grade': 1.9},
-        {'course': 'Geschichte', 'grade': 3.3},
-        # ...
-    ]
 
+    own_grades = get_grades(request.user.email)
+
+    # Entferne Duplikate basierend auf dem Modulnamen
+    unique_modules = []
+    for module in own_grades:
+        module_name = module['module_name']
+        
+        # Überprüfe, ob das Modul bereits in unique_modules ist
+        if not any(existing_module['module_name'] == module_name for existing_module in unique_modules):
+            unique_modules.append(module)
+
+    # Aktualisiere own_grades mit den eindeutigen Modulen
+    own_grades = unique_modules
+
+    total_average = calculate_total_average_weighted(request.user.email)
+
+
+    for module in own_grades:
+        for unit in module['units']:
+            # Ersetze None-Werte durch 0
+            for key in unit:
+                if unit[key] is None:
+                    unit[key] = 0
+            # Append grade distribution
+            unit['grade_distribution'] = get_grade_distribution_as_dict(unit['unit_id'])
+            # Append failure rate
+            unit['failure_rate'] = get_failure_rate_first_attempt(unit['unit_id'])
+            # Append passing rate
+            unit['passing_rate'] = get_passing_rate_first_attempt(unit['unit_id'])
+    # Append total average
+    own_grades.append({'total_average': total_average})
+    
+    
+    print(json.dumps(own_grades,indent=4))
     context = {'own_grades': own_grades}
     return render(request, 'home/index.html', context)
 
