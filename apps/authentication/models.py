60ea0e68f5e3066a4f1789c0b0b95077653c9690
Johannes Peters <peters.j.jun@gmail.com>
2023-12-08 09:21:12 +0100

    remove unused imports

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,7 +1,6 @@
 """Models for the authentication app."""
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.db.models import Model
 
 from .managers import UserManager
 
